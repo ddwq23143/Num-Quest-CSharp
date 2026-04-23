0f2289646dd98d46bc002f6d4389e0a779da2662
ddwq23143 <alanov.danila.12@gmail.com>
2026-04-23 21:44:45 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,11 @@
+namespace RandomNum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Main main = new Main();
+            main.Run();
+        }
+    }
+}
